3a851de715a52e7e8d5be04f0ac84e382c9ba084
Giel Berkers <info@gielberkers.com>
2016-06-17 20:15:48 +0200

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+var gulp = require("gulp")
+var fs = require("fs")
+var browserify = require("browserify")
+var babelify = require("babelify")
+
+/**
+ * Watch for changes in JS files
+ */
+gulp.task("watch", function(){
+  gulp.watch("./src/**/*.js", function(evt){
+    console.log("Change detected in " + evt.path)
+    browserify({ debug: true })
+      .transform(babelify)
+      .require("./src/index.js", { entry: true })
+      .bundle()
+      .on("error", function (err) { console.log("Error: " + err.message) })
+      .pipe(fs.createWriteStream("./dist/index.js")
+    )
+  })
+})
---
+++ src/index.js
@@ -0,0 +1,1 @@
+// Entry point of all the JavaScript magic of the website
